b0d975e4f9d493a61db27530721989bf105110a6
Kirilcha <33288427+Kirilcha@users.noreply.github.com>
2018-03-06 23:08:09 +0700

    1

---
+++ Поехали/Kladez.cpp
@@ -0,0 +1,34 @@
+#include "Klad.h"
+#include "Aforizm.h"
+#include "Posl_Pogov.h"
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+Klad* Klad::In(ifstream &ifst)  
+{
+	Klad *klad;
+	int key;
+	ifst >> key;
+	
+	switch (key)  
+	{
+	case 1:
+		klad = new Aforizm;  
+		break;
+	case 2:
+		klad = new Posl_Pog;
+		break;
+	default:    
+		return 0;
+	}
+	ifst.get();
+	getline(ifst, klad->fraza);
+	klad->InData(ifst);
+	return klad;
+}
+string Klad::fr()
+{
+	return fraza;
+}
